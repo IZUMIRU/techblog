0690fe66814eda3bdc5565f8412095234d30bcfb
森泉亮介 <izumiruryousuke@gmail.com>
2017-10-19 20:21:01 +0900

    画像だけスクレイピングできない

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,7 +1,30 @@
 class Scraping
   def self.recruit_lifestyle
+    links = []
     agent = Mechanize.new
-    page = agent.get("http://engineer.recruit-lifestyle.co.jp/techblog/")
+    current_page = agent.get("http://engineer.recruit-lifestyle.co.jp/techblog/")
+    elements = current_page.search('.rls-box01')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+    
+    links.each do |link|
+      get_product('http://engineer.recruit-lifestyle.co.jp' + link)
+    end
+  end
+
+  def self.get_product(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    # image = page.at('.rls-p-eyecatch rls-p-eyecatch__article')[:url]
+    title = page.at('.rls-a-article__title').inner_text.strip
+    url = link
+    # image_url = page.at('.pictBox img')[:src]
+    
+    blog = Blog.create(image: image, title: title, url: url)
+    blog.save
+  end
+end
 
     # images = page.search('.rls-box01__image')
     # images.each do |image|
@@ -9,43 +32,3 @@ class Scraping
     #   blog = Blog.new(image: recruit_lifestyle_image)
     #   blog.save
     # end
-    
-    titles = page.search('.rls-box01__description')
-    titles.each do |title|
-      recruit_lifestyle_title = title.inner_text.strip
-
-      urls = page.search('.rls-box01')
-      urls.each do |url|
-        recruit_lifestyle_url = 'http://engineer.recruit-lifestyle.co.jp' + url.get_attribute('href')
-
-        Blog.create(title: recruit_lifestyle_title, url: recruit_lifestyle_url) unless Blog.first.url.present?
-      end
-    end
-  end
-end
-
-
-
-# def self.movie_urls
-#     links = []
-#     agent = Mechanize.new
-#     current_page = agent.get("http://eiga.com/now/")
-#     elements = current_page.search('.m_unit h3 a')
-#     elements.each do |ele|
-#         links << ele.get_attribute('href')
-#     end
-    
-#     links.each do |link|
-#         get_product('http://eiga.com' + link)
-#     end
-# end
-
-# def self.get_product(link)
-#     agent = Mechanize.new
-#     page = agent.get(link)
-#     title = page.at('.moveInfoBox h1').inner_text
-#     image_url = page.at('.pictBox img')[:src]
-    
-#     product = Product.new(title: title, image_url: image_url)
-#     product.save
-# end
