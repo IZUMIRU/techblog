e4773636f3dbc83d118a945e2edb6e727213d5b2
森泉亮介 <izumiruryousuke@gmail.com>
2017-10-22 21:16:07 +0900

    グループを綺麗に表示

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,97 +1,10 @@
 class Scraping
-  # ライフスタイル
-  def self.lifestyle
-    links = []
-    agent = Mechanize.new
-    current_page = agent.get("http://engineer.recruit-lifestyle.co.jp/techblog/")
-    elements = current_page.search('.rls-box01')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
-    end
-    
-    links.each do |link|
-      get_lifestyle('http://engineer.recruit-lifestyle.co.jp' + link)
-    end
-  end
+  # 人材メディア・派遣
+  ## 就職・転職
+  ### キャリア
 
-  def self.get_lifestyle(link)
-    agent = Mechanize.new
-    page = agent.get(link)
-    title = page.at('.rls-a-article__title').inner_text.strip
 
-    Blog.create(company: "ライフスタイル", title: title, url: link)
-  end
-
-  # マーケティングパートナーズ
-  def self.marketing_partners
-    links = []
-    agent = Mechanize.new
-    current_page = agent.get("https://tech.recruit-mp.co.jp")
-    elements = current_page.search('.widget__title a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
-    end
-    
-    links.each do |link|
-      get_marketing_partners(link)
-    end
-  end
-
-  def self.get_marketing_partners(link)
-    agent = Mechanize.new
-    page = agent.get(link)
-    title = page.at('#article-title').inner_text.strip
-
-    Blog.create(company: "マーケティングパートナーズ", title: title, url: link)
-  end
-
-  # テクノロジーズ
-  def self.technologies
-    links = []
-    agent = Mechanize.new
-    current_page = agent.get("https://recruit-tech.co.jp/blog/")
-    elements = current_page.search('.article-left a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
-    end
-    
-    links.each do |link|
-      get_technologies(link)
-    end
-  end
-
-  def self.get_technologies(link)
-    agent = Mechanize.new
-    page = agent.get(link)
-    title = page.at('.article-title').inner_text.strip
-
-    Blog.create(company: "テクノロジーズ", title: title, url: link)
-  end
-
-  # 住まいカンパニー
-  def self.sumai_company
-    links = []
-    agent = Mechanize.new
-    current_page = agent.get("http://tech.recruit-sumai.co.jp")
-    elements = current_page.search('.entry-title a')
-    elements.each do |ele|
-      links << ele.get_attribute('href')
-    end
-    
-    links.each do |link|
-      get_sumai_company(link)
-    end
-  end
-
-  def self.get_sumai_company(link)
-    agent = Mechanize.new
-    page = agent.get(link)
-    title = page.at('.bookmark').inner_text.strip
-
-    Blog.create(company: "住まいカンパニー", title: title, url: link)
-  end
-
-  # ジョブス
+  ### ジョブス
   def self.jobs
     links = []
     agent = Mechanize.new
@@ -115,6 +28,130 @@ class Scraping
 
     Blog.create(company: "ジョブズ", title: title, url: link)
   end
+
+  ## 人材派遣
+  ### スタッフィング
+
+
+  ### スタッフサービス・ホールディングス
+
+
+  ## 人材・組織開発
+  ### マネジメントソリューションズ
+
+
+  # 販促メディア
+  ## 日常消費
+  ### ライフスタイル
+  def self.lifestyle
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("http://engineer.recruit-lifestyle.co.jp/techblog/")
+    elements = current_page.search('.rls-box01')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+    
+    links.each do |link|
+      get_lifestyle('http://engineer.recruit-lifestyle.co.jp' + link)
+    end
+  end
+
+  def self.get_lifestyle(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.rls-a-article__title').inner_text.strip
+
+    Blog.create(company: "ライフスタイル", title: title, url: link)
+  end
+
+  ## ライフイベント
+  ### 住まいカンパニー
+  def self.sumai_company
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("http://tech.recruit-sumai.co.jp")
+    elements = current_page.search('.entry-title a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+    
+    links.each do |link|
+      get_sumai_company(link)
+    end
+  end
+
+  def self.get_sumai_company(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.bookmark').inner_text.strip
+
+    Blog.create(company: "住まいカンパニー", title: title, url: link)
+  end
+
+  ### マーケティングパートナーズ
+  def self.marketing_partners
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("https://tech.recruit-mp.co.jp")
+    elements = current_page.search('.widget__title a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+    
+    links.each do |link|
+      get_marketing_partners(link)
+    end
+  end
+
+  def self.get_marketing_partners(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('#article-title').inner_text.strip
+
+    Blog.create(company: "マーケティングパートナーズ", title: title, url: link)
+  end
+
+  # 横断機能
+  ## 経営推進
+  ### アドミニストレーション
+
+
+  ### ホールディングス
+
+
+  ## 事業推進
+  ### コミュニケーションズ
+
+
+  ### テクノロジーズ
+  def self.technologies
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("https://recruit-tech.co.jp/blog/")
+    elements = current_page.search('.article-left a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+    
+    links.each do |link|
+      get_technologies(link)
+    end
+  end
+
+  def self.get_technologies(link)
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.article-title').inner_text.strip
+
+    Blog.create(company: "テクノロジーズ", title: title, url: link)
+  end
+
+
+
+
+
+
 
 
 
